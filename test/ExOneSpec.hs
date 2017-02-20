7703d56966bfa61b6ee682a0cb7630dcecedf252
@abinr <abinr@apotropaix.me>
2017-02-20 20:22:58 +0100

    now polymorphic

--- test/ExOneSpec.hs
+++ test/ExOneSpec.hs
@@ -12,14 +12,16 @@ spec = do
   describe "last as Maybe" $ do
     it "should return the last element of a list" $ do
       lastAsMaybe [0..5] `shouldBe` Just 5
+    it "should be polymorphic" $ do
+      lastAsMaybe ['a'..'d'] `shouldBe` Just 'd'
     it "should return Nothing for an empty list" $ do
-      lastAsMaybe [] `shouldBe` Nothing
+      lastAsMaybe ([] :: [Int]) `shouldBe` Nothing
     it "should return a Maybe result for an arbitrary list" $
       property $ \xs -> lastAsMaybe xs == f (xs :: [Int])
 
 f = (fmap fst) . uncons . reverse
 
-lastAsMaybe :: [Int] -> Maybe Int
+lastAsMaybe :: [a] -> Maybe a
 lastAsMaybe list =
   case list of
     [] ->
--- test/ExTwoSpec.hs
+++ test/ExTwoSpec.hs
@@ -12,16 +12,18 @@ spec = do
   describe "next to last as Maybe" $ do
     it "should return first when length is 2" $ do
       nextToLastAsMaybe [4, 5] `shouldBe` Just 4
+    it "should be polymorphic" $ do
+      nextToLastAsMaybe ['a'..'e'] `shouldBe` Just 'd'
     it "should return Nothing when length is 1" $ do
       nextToLastAsMaybe [9] `shouldBe` Nothing
     it "should return Nothing when length is 0" $ do
-      nextToLastAsMaybe [] `shouldBe` Nothing
+      nextToLastAsMaybe ([]::[Int]) `shouldBe` Nothing
     it "should return Maybe for arbitray list" $
-      property $ \xs -> nextToLastAsMaybe xs == f xs
+      property $ \xs -> nextToLastAsMaybe xs == f (xs :: [Int])
 
 f = fst . foldl (\(a,b) x -> (b, Just x)) (Nothing, Nothing)
 
-nextToLastAsMaybe :: [Int] -> Maybe Int
+nextToLastAsMaybe :: [a] -> Maybe a
 nextToLastAsMaybe list =
   case list of
     [] ->
