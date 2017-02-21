450ed6bde48f7782f0db3465474a5e5e5b11658e
@abinr <abinr@apotropaix.me>
2017-02-21 22:11:16 +0100

    Added Ex 3

---
+++ test/ExThreeSpec.hs
@@ -0,0 +1,27 @@
+module ExThreeSpec (main, spec) where
+
+import Test.Hspec
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "Find the Kth element of a list. First element is number 1" $ do
+    it "should find the Kth element as a Maybe" $ do
+      elementAt [1,2,3] 2 `shouldBe` Just 2
+    it "should return Nothing for empty list" $ do
+      elementAt ([]::[Char]) 2 `shouldBe` Nothing
+    it "should return Nothing when k < 1" $ do
+      elementAt [1] 0 `shouldBe` Nothing
+    it "should return Nothing when k < length list" $ do
+      elementAt [1] 2 `shouldBe` Nothing
+    it "should be polymorphic" $ do
+      elementAt ['a'..'z'] 24 `shouldBe` Just 'x'
+
+elementAt :: [a] -> Int -> Maybe a
+elementAt [] _ = Nothing
+elementAt (x:_) 1 = Just x
+elementAt (_:xs) k
+  | k < 1 = Nothing
+  | otherwise = elementAt xs (k - 1)
