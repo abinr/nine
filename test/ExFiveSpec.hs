6294da6659a4b06fefb24e98c8b790203a0cbffa
@abinr <brianfarrar@icloud.com>
2017-02-25 19:58:09 +0100

    add foldr for fun

--- test/ExFiveSpec.hs
+++ test/ExFiveSpec.hs
@@ -12,7 +12,10 @@ spec = do
     it "should equal itself after reversing twice" $
       property $ \xs -> (myReverse . myReverse) xs == (xs :: [Int]) 
     it "should be polymorphic" $
-      property $ \xs -> (myReverse . myReverse) xs == (xs :: [Char]) 
+      property $ \xs -> (f . myReverse) xs == (xs :: [Char]) 
+
+{-- for fun --}
+f xs = foldr (\x fId empty -> fId (x : empty)) id xs []
 
 myReverse :: [a] -> [a]
 myReverse list =
