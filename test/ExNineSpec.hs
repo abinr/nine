45fd946531a45733afdd0b4294bae28f3f1cee03
@abinr <brianfarrar@icloud.com>
2017-04-25 15:30:53 +0200

    added exc. nine

---
+++ test/ExNineSpec.hs
@@ -0,0 +1,32 @@
+module ExNineSpec (main, spec) where
+
+import Test.Hspec
+
+main :: IO ()
+main = do
+  hspec spec
+
+spec :: Spec
+spec = do
+  describe "Pack consecutive duplicates into sublists" $ do
+    it "should return an empty list for an empty list" $ do
+      pack [] `shouldBe` ([] :: [[Int]])
+    it "should return a single element list for the same" $ do
+      pack [1] `shouldBe` ([[1]])
+    it "should return sublists for unique elements" $ do
+      pack [1, 2] `shouldBe` [[1], [2]]
+    it "should pack consecutive dupls into sublists" $ do
+      pack [1,1,2] `shouldBe` [[1,1], [2]]
+    it "should be polymorphic" $ do
+      pack ['a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e']
+       `shouldBe` ["aaaa","b","cc","aa","d","eeee"]
+
+
+pack :: Eq a => [a] -> [[a]]
+pack xs =
+  case xs of
+    [] ->
+      []
+    x:xs ->
+      (x : takeWhile (==x) xs) : pack (dropWhile (==x) xs)
+
